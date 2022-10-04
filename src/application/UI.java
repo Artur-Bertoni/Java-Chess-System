a720b28d0b347a5b5391b5e0dcf93854b4fb9388
Artur-Bertoni <artur_b_santos@hotmail.com>
2022-10-04 12:00:35 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -18,11 +18,20 @@ public class UI {
 
     private static void printPiece(ChessPiece piece){
         if (piece == null){
-            System.out.print("-");
+            System.out.print("⚊");
         }
         else{
             System.out.print(piece);
         }
         System.out.print(" ");
     }
+
+    /*
+    * torre: ♜ ----
+    * cavalo: ♞
+    * bispo: ♝
+    * rei: ♚ ----
+    * rainha: ♛
+    * peão: ♙
+    * */
 }
